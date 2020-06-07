cfc81568c9eb6a76985d425c6a7533cb22dce76f
DeltaByte <thedeltabyte@gmail.com>
2020-06-07 06:29:55 +0100

    add cognito user pool

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,18 +1,24 @@
 resource "aws_cognito_user_pool" "default" {
   name = "mediacodex"
 
-  //from = "noreply@${var.domain}"
-  // TODO: mfa_configuration = "OPTIONAL"
+  // Email
+  email_configuration {
+    email_sending_account = "DEVELOPER"
+    source_arn            = aws_ses_domain_identity.default.arn
+    //from_email_address    = "noreply@${local.domain}"
+  }
 
+  // Devices
   device_configuration {
     challenge_required_on_new_device      = true
     device_only_remembered_on_user_prompt = true
   }
 
-  /*email_configuration {
-    email_sending_account = "DEVELOPER"
-    source_arn            = aws_ses_domain_identity.default.arn
-  }*/
+  // Software MFA
+  mfa_configuration = "OPTIONAL"
+  software_token_mfa_configuration {
+    enabled = true
+  }
 
   // https://docs.microsoft.com/en-us/office365/admin/misc/password-policy-recommendations
   // https://pages.nist.gov/800-63-3/sp800-63b.html
