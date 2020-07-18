dd148d71ac748a464d9b7423f841ee98fad16781
DeltaByte <thedeltabyte@gmail.com>
2020-07-18 17:01:57 +0100

    setup cloudflare SSL

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,6 +3,16 @@
 resource "cloudflare_zone" "main" {
   zone = local.domain
 }
+resource "cloudflare_zone_settings_override" "main" {
+  zone_id = cloudflare_zone.main.id
+  settings {
+    ssl = "strict"
+    tls_1_3 = "on"
+    min_tls_version = "1.2"
+    always_use_https = "on"
+    automatic_https_rewrites = "on"
+  }
+}
 
 /*
  * Webmail
