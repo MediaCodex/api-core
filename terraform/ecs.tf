3053edc31fbf7d6b8390fb116d4a747a59446bdc
DeltaByte <thedeltabyte@gmail.com>
2020-07-31 16:48:30 +0100

    remove placeholder cloudmap service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -20,30 +20,11 @@ resource "aws_service_discovery_private_dns_namespace" "ecs_api" {
   vpc  = aws_vpc.ecs_api.id
 }
 
-resource "aws_service_discovery_service" "ecs_api" {
-  name = local.api_cluster_name
-
-  dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.ecs_api.id
-
-    dns_records {
-      ttl  = 10
-      type = "SRV"
-    }
-
-    routing_policy = "MULTIVALUE"
-  }
-
-  health_check_custom_config {
-    failure_threshold = 1
-  }
-}
-
 /*
  * Outputs
  */
-output "ecs_api_discovery" {
-  value = aws_service_discovery_service.ecs_api.arn
+output "ecs_api_namespace" {
+  value = aws_service_discovery_private_dns_namespace.ecs_api.id
 }
 
 output "ecs_api_cluster" {
