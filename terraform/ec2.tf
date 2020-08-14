79a0a9658614f1e19f12eed41b0b621b6ea6cc40
DeltaByte <thedeltabyte@gmail.com>
2020-08-14 20:03:23 +0100

    switch to deversied allocation

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -104,7 +104,7 @@ resource "aws_security_group" "ecs_api" {
 resource "aws_spot_fleet_request" "ecs_api" {
   spot_price          = "0.03"
   fleet_type          = "maintain"
-  allocation_strategy = "lowestPrice"
+  allocation_strategy = "diversified"
   iam_fleet_role      = aws_iam_role.ecs_spot_fleet.arn
   target_capacity     = lookup(var.ecs_capacity, local.environment)
 
